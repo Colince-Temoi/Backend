f506b5207063ea607083cb70aacf3b57fd6fe185
colince.temoi <colince.temoi@host.docker.internal>
2024-02-29 15:12:29 +0300

    GamingApp
    Have just created the structure in place.

---
+++ Boot/Lobby/GamingApp/src/main/java/tech/csm/GamingAppApplication.java
@@ -0,0 +1,13 @@
+package tech.csm;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GamingAppApplication.class, args);
+	}
+
+}
