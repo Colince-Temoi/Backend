71c2859a41443800234e6e31fb442162380ffd99
TMI_7 <tmi2023@gamil.com>
2024-10-09 10:33:47 +0300

    Proper CSRF solution implementation

--- Spring/Security/section3/springsecsection3/src/main/java/com/eazybytes/EazyBankBackendApplication.java
+++ Spring/Security/section3/springsecsection3/src/main/java/com/eazybytes/EazyBankBackendApplication.java
@@ -8,6 +8,6 @@ public class EazyBankBackendApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(EazyBankBackendApplication.class, args);
-    }
+    }   
 
 }
--- Spring/Security/section3/springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
+++ Spring/Security/section3/springsecsection3/src/main/java/com/eazybytes/config/ProjectSecurityConfig.java
@@ -1,16 +1,21 @@
 package com.eazybytes.config;
 
+import com.eazybytes.filter.CsrfCookieFilter;
 import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
+import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
+import org.springframework.security.web.csrf.CsrfTokenRequestAttributeHandler;
 import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.CorsConfigurationSource;
 
@@ -25,14 +30,11 @@ public class ProjectSecurityConfig {
 
     @Bean
     SecurityFilterChain defaultSecurityFilterChain(HttpSecurity http) throws Exception {
-        /* What we have done is we have disabled the CSRF protection because previously when we tried to access the /register
-        endpoint Spring Security was stopping us with a 403 error (Forbidden!) by thinking it is a CSRF attack!
-        * But this is not the recommended approach inside production; we should never disable CSRF protection.
-        * Instead, we should ideally implement a proper CSRF solution for our web application
-        * To test the default CSRF protection, we can comment out the below line and try to access the /register endpoint or any other modify endpoints! both will give us a 403 error
-        * This is whether you are using a UI application or Postman to test the endpoints.
-        */
-        http.cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
+        CsrfTokenRequestAttributeHandler requestHandler = new CsrfTokenRequestAttributeHandler();
+        http.securityContext((context) -> context
+                        .requireExplicitSave(false))
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.ALWAYS))
+                .cors(corsCustomizer -> corsCustomizer.configurationSource(new CorsConfigurationSource() {
                     @Override
                     public CorsConfiguration getCorsConfiguration(HttpServletRequest request) {
                         CorsConfiguration config = new CorsConfiguration();
@@ -44,10 +46,12 @@ public class ProjectSecurityConfig {
                         return config;
                     }
                 }))
-//                .csrf(AbstractHttpConfigurer::disable)
+                .csrf((csrf) -> csrf.csrfTokenRequestHandler(requestHandler).ignoringRequestMatchers( "/register","/contact")
+                        .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse()))
+                .addFilterAfter(new CsrfCookieFilter(), BasicAuthenticationFilter.class)
                 .authorizeHttpRequests((requests) -> requests
-                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards","/user" ).authenticated()
-                .requestMatchers("/notices", "/contact", "/error","/register").permitAll());
+                .requestMatchers("/myAccount", "/myBalance", "/myLoans", "/myCards", "/user" ).authenticated()
+                .requestMatchers("/notices","/contact", "/error","/register").permitAll()); 
         http.formLogin(withDefaults());
         http.httpBasic(withDefaults());
         return http.build();
