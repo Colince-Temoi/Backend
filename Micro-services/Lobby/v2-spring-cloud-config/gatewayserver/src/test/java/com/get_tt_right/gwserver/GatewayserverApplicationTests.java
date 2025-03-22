5457e41a3f3ff6397255815e49703b30f3a707e4
TMI_7 <tmi2023@gamil.com>
2025-03-22 07:44:03 +0300

    Edge Server set up  with default routing configs + visual demo and discussions - Added missing files

---
+++ Micro-services/Lobby/v2-spring-cloud-config/gatewayserver/src/test/java/com/get_tt_right/gwserver/GatewayserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.get_tt_right.gwserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GatewayserverApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
